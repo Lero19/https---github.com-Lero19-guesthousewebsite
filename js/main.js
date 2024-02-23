1d7205fa8b4563a3dfbe9cd88f80b5361c0440c3
Nkosingiphile Ncongwane <Leroylunaticsane19@gmail.com>
2024-02-24 00:46:26 +0200

    4th commit

--- js/main.js
+++ js/main.js
@@ -29,3 +29,25 @@ var swiper = new Swiper(".mySwiper", {
         delay: 3500,
     },
 });
+
+// counter code
+document.addEventListener("DOMContentLoaded", () =>{
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increament = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increament;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0 , 128, 3000);
+    counter("count2", 100 , 578, 2500);
+    counter("count3", 0 , 1400, 300);
+    counter("count4", 0 , 711, 3000);
+})
